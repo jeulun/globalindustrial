8ad2cd41b87a989d8c9e43170d9168834a7d95d0
tohjeulun <tohjeulun@gmail.com>
2018-07-25 01:03:07 +0800

    Added my project

--- app/helpers.php
+++ app/helpers.php
@@ -14,3 +14,17 @@ function productImage($path)
 {
     return $path && file_exists('storage/'.$path) ? asset('storage/'.$path) : asset('img/not-found.jpg');
 }
+
+
+function getStockLevel($quantity)
+{
+    if ($quantity > setting('site.stock_threshold')) {
+        $stockLevel = '<div class="badge badge-success">In Stock</div>';
+    } elseif ($quantity <= setting('site.stock_threshold') && $quantity > 0) {
+        $stockLevel = '<div class="badge badge-warning">Low Stock</div>';
+    } else {
+        $stockLevel = '<div class="badge badge-danger">Not available</div>';
+    }
+
+    return $stockLevel;
+}
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -40,6 +40,7 @@
         <div class="product-section-information">
             <h1 class="product-section-title">{{ $product->name }}</h1>
             <div class="product-section-subtitle">{{ $product->details }}</div>
+            <div>{!! $stockLevel !!}</div>
             <div class="product-section-price">{{ $product->presentPrice() }}</div>
 
             @if ($product->dimensionimage)
