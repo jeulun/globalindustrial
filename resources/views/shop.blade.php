376a62bb383d3cb728f4dff5f996289d7a4ca1c1
tohjeulun <tohjeulun@gmail.com>
2018-07-29 12:33:57 +0800

    Added my project

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -34,7 +34,15 @@
         </div> <!-- end sidebar -->
         <div>
             <div class="products-header">
+                <h1 class="stylish-heading">
                 
+                
+                {{ $categoryName }}
+                
+
+
+                </h1>
+              
             </div>
 
             <div class="products text-center">
